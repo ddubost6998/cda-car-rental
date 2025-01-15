963f575e9ba5447dfabd1aea5f2ee189ea58aa95
Damien <ddubost6998@gmail.com>
2025-01-15 16:54:21 +0100

    Add informations for customers form

--- src/main/java/fr/hb/car_rental/config/InternationalizationConfig.java
+++ src/main/java/fr/hb/car_rental/config/InternationalizationConfig.java
@@ -1,5 +1,7 @@
 package fr.hb.car_rental.config;
 
+import java.util.Locale;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +11,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class InternationalizationConfig implements WebMvcConfigurer {
