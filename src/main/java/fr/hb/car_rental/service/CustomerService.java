63cd93e3b54176502ca033010448e971d57c73a2
Damien <ddubost6998@gmail.com>
2025-01-15 14:18:26 +0100

    Add Customer service

---
+++ src/main/java/fr/hb/car_rental/service/CustomerService.java
@@ -0,0 +1,14 @@
+package fr.hb.car_rental.service;
+
+import fr.hb.car_rental.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private static final List<Customer> customerList = new ArrayList<>();
+
+}
